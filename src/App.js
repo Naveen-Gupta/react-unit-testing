670d1d35bdc32ab90a8956b000ff595a7fbbe5dd
nvngupta1992 <nvngupta1992@gmail.com>
2020-01-19 15:23:26 +0530

    removed DRY code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App" data-test="component-app"></div>;
+    return (<div className="App" data-test="component-app">
+    <h1 data-test="counter-display">Current Counter value: </h1>
+    <button data-test="increment-button" />
+    </div>);
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -6,16 +6,43 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter()});
 
+/**
+  * Factory function to create a ShallowWrapper for te App component.
+  * @function setup
+  * @param {object} props - Component props specific to this setup.
+  * @param {any} state - Initial state for setup
+  * @returns {ShallowWrapper}
+ */
+const setup = (props={}, state=null) => {
+  return shallow(<App {...props} />);
+}
+
+/**
+ * Retruns ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute for serach.
+ * @returns {ShallowWrapper}
+ */ 
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+}
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
-  expect(appComponent.length).toBe(1); /** passed */
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper,'component-app');
+  expect(appComponent.length).toBe(1); 
 });
 
 test('renders increment button', () => {
+  const wrapper = setup();
+  const incrementButton = findByTestAttr(wrapper,'increment-button');
+  expect(incrementButton.length).toBe(1); 
 });
 
 test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper,'counter-display');
+  expect(counterDisplay.length).toBe(1); 
 });
 
 test('counter display starts with zero', () => {
